9a0090dceb1dad464126d161d84b552d935f5e1e
Edward Kim <itgma1231@gmail.com>
2023-11-08 12:28:45 -0500

    apollo client setup; browser router outlet page created

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,17 +2,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
 
 import App from "./App.jsx";
 import ErrorPage from "./pages/ErrorPage.jsx";
-import "semantic-ui-css/semantic.min.css";
+import SearchNews from "./pages/SearchNews.jsx";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     errorElement: <ErrorPage />,
-    children: []
+    children: [
+      {
+        index: true,
+        element: <SearchNews />
+      }
+    ]
   }
 ])
 
---
+++ client/src/pages/SearchNews.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const SearchNews = () => {
+  return (
+    <div>SearchNews</div>
+  )
+}
+
+export default SearchNews
