dc2cfedf5ce8d2fc9e49ec3fc1f7d5c750a1c4bc
samantha gard <samantha.gard13@gmail.com>
2023-11-07 21:29:30 -0500

    react program added to client folder/beginning navbar

---
+++ client/Headline-Report/src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#login">Log In</a></li>
+                <li><a href="#profile">Profile</a></li>
+            </ul>
+        </nav>
+    )
+};
+    
+export default NavBar;
