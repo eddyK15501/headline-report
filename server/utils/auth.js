7017dc777d90697fb088d8f26f9afe44e385c75e
Edward Kim <itgma1231@gmail.com>
2023-11-07 19:20:08 -0500

    /utils/auth.js

---
+++ server/utils/auth.js
@@ -0,0 +1,36 @@
+const { GraphQLError } = require("graphql");
+const jwt = require("jsonwebtoken");
+require('dotenv').config()
+
+module.exports = {
+  AuthenticationError: new GraphQLError("Could not authenticate user.", {
+    extensions: {
+      code: "UNAUTHENTICATED",
+    },
+  }),
+  authMiddleware: function ({ req, res, next }) {
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    try {
+      const { data } = jwt.verify(token, process.env.SECRET_KEY, { maxAge: process.env.EXPIRATION });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, process.env.SECRET_KEY, { expiresIn: process.env.EXPIRATION });
+  },
+};
