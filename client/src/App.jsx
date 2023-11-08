fb9c4951df5b823b22d36c17a3ed5ee3cb65656f
Edward Kim <itgma1231@gmail.com>
2023-11-08 12:17:26 -0500

    auth.js/jwt-decode client-side user authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,31 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Outlet } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  
+})
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Outlet />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
