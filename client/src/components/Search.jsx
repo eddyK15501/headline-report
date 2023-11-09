f93ded397a5f2f8c55a0cfb119fecb8c4547c910
Edward Kim <itgma1231@gmail.com>
2023-11-09 02:03:57 -0500

    npm install -D sass; google fonts added

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 
 const Search = () => {
@@ -6,11 +7,11 @@ const Search = () => {
             
             <div style={{ backgroundColor: '#3D3D3D', color: 'white', padding: "1em" }}>
 
-            <h2 style={{display: 'flex', justifyContent: "center", alignItems: 'center' }}>Start A New Search</h2>
+            {/* <h2 style={{display: 'flex', justifyContent: "center", alignItems: 'center' }}>Start A New Search</h2> */}
 
             <div style={{display: 'flex', justifyContent: "center", alignItems: 'center'}}>
                 
-                <input style={{borderRadius: ".5em"}} placeholder="Crime...Stocks..."></input>
+                <input style={{borderRadius: ".5em"}} placeholder="Search for news"></input>
                 <button style={{backgroundColor: "white", borderRadius: ".5em"}}>Search</button>
             </div>
         </div>
