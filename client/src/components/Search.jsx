cb573753859eb5db2a93688fa13e3910dd40f9e9
Edward Kim <itgma1231@gmail.com>
2023-11-09 14:18:01 -0500

    update Homepage search results array, from the Search component

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,23 +1,60 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import { useState } from "react";
+import searchNews from "../../utils/API";
 
-const Search = () => {
-    return (
-        <div style={{padding: "1em"}}>
-            
-            <div style={{ backgroundColor: '#3D3D3D', color: 'white', padding: "1em" }}>
+const Search = ({ handleSearchInput }) => {
+  const [searchInput, setSearchInput] = useState("");
 
-            {/* <h2 style={{display: 'flex', justifyContent: "center", alignItems: 'center' }}>Start A New Search</h2> */}
+  const onFormSubmit = async (event) => {
+    event.preventDefault();
 
-            <div style={{display: 'flex', justifyContent: "center", alignItems: 'center'}}>
-                
-                <input style={{borderRadius: ".5em"}} placeholder="Search for news"></input>
-                <button style={{backgroundColor: "white", borderRadius: ".5em"}}>Search</button>
-            </div>
+    try {
+      const response = await searchNews(searchInput);
+
+      if (!response.ok) {
+        throw new Error("something went wrong!");
+      }
+      const { articles } = await response.json();
+
+      handleSearchInput(articles);
+    } catch (err) {
+      console.error("Error fetching data:", err);
+    } finally {
+      setSearchInput("");
+    }
+  };
+
+  return (
+    <div style={{ padding: "1em" }}>
+      <div
+        style={{ backgroundColor: "#3D3D3D", color: "white", padding: "1em" }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <form onSubmit={onFormSubmit}>
+            <input
+              style={{ borderRadius: ".5em" }}
+              placeholder="Search for news"
+              onChange={(e) => setSearchInput(e.target.value)}
+              value={searchInput}
+            ></input>
+            <button
+              style={{ backgroundColor: "white", borderRadius: ".5em" }}
+              type="submit"
+            >
+              Search
+            </button>
+          </form>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
-
-export default Search
+export default Search;
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,20 +1,19 @@
 /* eslint-disable no-unused-vars */
-import { useEffect } from "react";
-import searchNews from "../../utils/API";
+import { useState } from "react";
 
 import Search from "../components/Search";
 
 const Homepage = () => {
-  // THIS IS JUST TEMPORARY; uncomment useEffect() to get News API data in console.log()
-  //   useEffect(() => {
-  //     searchNews("today")
-  //       .then((res) => res.json())
-  //       .then(({ articles }) => console.log(articles));
-  //   }, []);
+  const [searchedNews, setSearchedNews] = useState([])
+
+  const handleSearchInput = (search) => {
+    setSearchedNews(search)
+    console.log(search)
+  }
 
   return (
     <div style={{ margin: "1em" }}>
-        <Search />
+        <Search handleSearchInput={handleSearchInput} />
     </div>
   )
 };
