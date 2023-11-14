bddc8c49d2232ec7ea8208c0fe431b800db366db
Edward Kim <itgma1231@gmail.com>
2023-11-14 15:33:34 -0500

    process.env.NODE_ENV === production

--- server/server.js
+++ server/server.js
@@ -27,12 +27,12 @@ const startServer = async () => {
     context: authMiddleware 
   }));
 
-  // if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/dist")));
     app.get("*", (req, res) => {
       res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
-  // }
+  }
 
   db.once("open", () => {
     app.listen(PORT, () => {
